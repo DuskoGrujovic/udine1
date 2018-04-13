b1f0aed694644ff2c7183dd27d61ef70eca09557
Dusko Grujovic <dusko@Duskos-MacBook-Pro.local>
2018-04-13 06:58:34 +0200

    needed: icons integrtion, removing unused css & javascript codes, responsive...

--- udine1/js/custom.js
+++ udine1/js/custom.js
@@ -1,7 +1,67 @@
+$(document).ready(function() {
+$('.search-query').blur(function() {
+    $(this).closest('.form-search').removeClass('search-active');
+  })
+  .focus(function() {
+    $(this).closest('.form-search').addClass('search-active');
+  });
+});
+
+//Angular Carousel
+
+angular.module('BtApp', [
+            'angular-carousel'
+        ])
+
+        .controller('CarouselAngCtrl', function($scope) {
+
+
+            function getSlide(target, style) {
+                var i = target.length;
+                return {
+                    id: (i + 1),
+                    label: 'slide #' + (i + 1),
+                    img: 'img/slides' + '/' + ((i + 1) % 10)+ '.jpg' ,
+                    color: "#fff",
+                    odd: (i % 2 === 0),
+                };
+            }
+
+            function addSlide(target, style) {
+                target.push(getSlide(target, style));
+            };
+
+
+            $scope.carouselIndex2 = 0;
+
+
+
+            function addSlides(target, style, qty) {
+                for (var i=0; i < qty; i++) {
+                    addSlide(target, style);
+                }
+            }
+
+
+
+            // 2nd ngRepeat demo
+            $scope.slides = [];
+            addSlides($scope.slides, '', 4);
+
+
+            // End to End swiping
+            // load 130 images in main javascript container
+            var slideImages = [];
+            $scope.totalimg = slideImages.length;
+            $scope.galleryNumber = 1;
+        })
+
+ //Angular Carousel END
+
+
 /*global jQuery:false */
 jQuery(document).ready(function($) {
-  "use strict";
-
+  "use strict"
 
   (function() {
 
@@ -230,4 +290,59 @@ jQuery(document).ready(function($) {
     google.maps.event.addDomListener(window, 'load', initialize_google_map);
   }
 
+});
+
+// Azienda counter 
+$('.count-this').each(function () {
+
+	// Start the counting from a specified number - in this case, 0!
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+    	// Speed of counter in ms, default animation style
+        duration: 2000,
+        easing: 'swing',
+		format:9999,
+        step: function (now) {
+        	// Round up the number
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+$(document).ready(function() {
+	
+    var numberValue1 = $('#count-1').val();
+	var numberValue2 = $('#count-2').val();
+	var numberValue3 = $('#count-3').val();
+	if (numberValue1 > 10 && numberValue1 < 100) {
+		$('#count-container-1 .zero').append(0);
+		$('#count-container-1 .zero').append(0);
+	} else if (99 < numberValue1 && numberValue1 < 1000 ){
+		$('#count-container-1 .zero').append(0);
+	} else if (numberValue1 < 10) {
+		$('#count-container-1 .zero').append(0);
+		$('#count-container-1 .zero').append(0);
+		$('#count-container-1 .zero').append(0);
+	}
+	if (numberValue2 > 10 && numberValue2 < 100) {
+		$('#count-container-2 .zero').append(0);
+		$('#count-container-2 .zero').append(0);
+	} else if (99 < numberValue2 && numberValue2 < 1000 ){
+		$('#count-container-2 .zero').append(0);
+	} else if (numberValue2 < 10) {
+		$('#count-container-2 .zero').append(0);
+		$('#count-container-2 .zero').append(0);
+		$('#count-container-2 .zero').append(0);
+	}
+	if (numberValue3 > 10 && numberValue3 < 100) {
+		$('#count-container-3 .zero').append(0);
+		$('#count-container-3 .zero').append(0);
+	} else if (99 < numberValue3 && numberValue3 < 1000 ){
+		$('#count-container-3 .zero').append(0);
+	} else if (numberValue3 < 10) {
+		$('#count-container-3 .zero').append(0);
+		$('#count-container-3 .zero').append(0);
+		$('#count-container-3 .zero').append(0);
+	}
+	
 });
