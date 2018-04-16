c4aeb5d0c8e9d9924c8fbcbb6d560d4db38efbd2
MatijaMilovanovic <matija.milovanovic@capsule.rs>
2018-04-16 12:07:03 +0200

    Sticky header added

--- udine1/js/custom.js
+++ udine1/js/custom.js
@@ -6,3 +6,17 @@ $('.search-query').blur(function() {
     $(this).closest('.form-search').addClass('search-active');
   });
 });
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop + 100;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
