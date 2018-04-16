12f51e4880be115e24aa6cd33c3d4d47563ab308
dusko <dusko@eklipsa.net>
2018-04-16 15:52:39 +0200

    Added sticky header

--- udine1/js/custom.js
+++ udine1/js/custom.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 $('.search-query').blur(function() {
     $(this).closest('.form-search').removeClass('search-active');
@@ -6,6 +7,20 @@ $('.search-query').blur(function() {
     $(this).closest('.form-search').addClass('search-active');
   });
 });
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop + 100;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
 // Azienda counter
 
