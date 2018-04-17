d7757ca73e3bd9d49d2bcd3fa9f76c287c6ac599
MatijaMilovanovic <matija.milovanovic@capsule.rs>
2018-04-17 18:34:51 +0200

    Megamenu added
    Needs minor fixes

--- udine1/js/custom.js
+++ udine1/js/custom.js
@@ -1,11 +1,17 @@
 
 $(document).ready(function() {
-$('.search-query').blur(function() {
-    $(this).closest('.form-search').removeClass('search-active');
-  })
-  .focus(function() {
-    $(this).closest('.form-search').addClass('search-active');
-  });
+    $(".dropdown").hover(
+        function() { $('.dropdown-menu', this).fadeIn("fast");
+        },
+        function() { $('.dropdown-menu', this).fadeOut("fast");
+    });
+    
+    $('.search-query').blur(function() {
+        $(this).closest('.form-search').removeClass('search-active');
+      })
+      .focus(function() {
+        $(this).closest('.form-search').addClass('search-active');
+      });
 });
 
 window.onscroll = function() {myFunction()};
