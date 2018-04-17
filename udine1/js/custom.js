13882301328762b810af5cf7706c239eaaafdb9e
MatijaMilovanovic <matija.milovanovic@capsule.rs>
2018-04-17 15:46:32 +0200

    Majoy fixes

--- udine1/js/custom.js
+++ udine1/js/custom.js
@@ -12,11 +12,11 @@ window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
 
-var sticky = navbar.offsetTop + 100;
+var sticky = navbar.offsetTop + 110;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
